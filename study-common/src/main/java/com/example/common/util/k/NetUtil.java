20ab0c2bde354979b708abb91c556cd9ef6188a3
fuzhi <fuzhi@sinosoft.com.cn>
2021-01-06 16:57:50 +0800

    好用的util

---
+++ study-common/src/main/java/com/example/common/util/k/NetUtil.java
@@ -0,0 +1,68 @@
+package com.example.common.util.k;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * net util
+ *
+ * @author xuxueli 2017-11-29 17:00:25
+ */
+@Slf4j
+public class NetUtil {
+    /**
+     * find avaliable port
+     *
+     * @param defaultPort
+     * @return
+     */
+    public static int findAvailablePort(int defaultPort) {
+        int portTmp = defaultPort;
+        while (portTmp < 65535) {
+            if (!isPortUsed(portTmp)) {
+                return portTmp;
+            } else {
+                portTmp++;
+            }
+        }
+        portTmp = defaultPort--;
+        while (portTmp > 0) {
+            if (!isPortUsed(portTmp)) {
+                return portTmp;
+            } else {
+                portTmp--;
+            }
+        }
+        throw new RuntimeException("no available port.");
+    }
+
+    /**
+     * check port used
+     *
+     * @param port
+     * @return
+     */
+    public static boolean isPortUsed(int port) {
+        boolean used = false;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            used = false;
+        } catch (IOException e) {
+            log.info(">>>>>>>>>>> xxl-rpc, port[{}] is in use.", port);
+            used = true;
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    log.info("");
+                }
+            }
+        }
+        return used;
+    }
+
+}
---
+++ study-common/src/main/java/com/example/common/util/k/ThrowableUtil.java
@@ -0,0 +1,23 @@
+package com.example.common.util.k;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ */
+public class ThrowableUtil {
+
+    /**
+     * parse error to string
+     *
+     * @param e
+     * @return
+     */
+    public static String toString(Throwable e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        String errorMsg = stringWriter.toString();
+        return errorMsg;
+    }
+
+}
